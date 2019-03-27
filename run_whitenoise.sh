5f04957257b5d90853ae93f3385d19a37e106e89
Shiqi Guo <384589859@qq.com>
2019-03-27 20:41:33 +0800

    init

---
+++ run_whitenoise.sh
@@ -0,0 +1,2 @@
+python main.py  -set scene -category noise64 -img_size 64 -lr_des 0.0003 -langevin_step_size_des 0.001 -langevin_step_num_des 10 -sigma_des 0.016 -num_epoch 500 -log_epoch 50 -batch_size 1 -nRow 12 -nCol 12 -data_path ./Texture/
+
